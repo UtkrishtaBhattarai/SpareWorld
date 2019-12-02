706495bc5d7fd107cb81a48c985b78b5b28f70f8
bhattarai <bhattarai.utk1@gmail.com>
2019-12-02 00:18:20 -0600

    Login and dashbboard

--- app/src/main/java/com/example/spareworld/MainActivity.java
+++ app/src/main/java/com/example/spareworld/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.spareworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etemail,etpwd;
+    Button btnlogin;
+    String name,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etemail=findViewById(R.id.etemail);
+        etpwd=findViewById(R.id.etpwd);
+        btnlogin=findViewById(R.id.btnlogin);
+        name=etemail.getText().toString();
+        password=etpwd.getText().toString();
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etemail.getText().toString().equals("admin") && etpwd.getText().toString().equals("admin"))
+                {
+                    Intent intent=new Intent(MainActivity.this,LandingActivity.class);
+                    startActivity(intent);
+
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
